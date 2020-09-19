10656d623976db16ee71477a4fdc3acf209b25d9
momaek <momaek17@gmail.com>
2020-09-19 17:52:19 +0800

    remove useless comment

--- cmd/fuzz.go
+++ cmd/fuzz.go
@@ -1,18 +1,3 @@
-/*
-Copyright © 2020 momaek17@gmail.com
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
 package cmd
 
 import (
@@ -54,6 +39,7 @@ type Token struct {
 	Period       int    `json:"period"`
 }
 
+// AlfredOutput alfred workflow output
 type AlfredOutput struct {
 	Title    string `json:"title"`
 	Subtitle string `json:"subtitle"`
@@ -181,6 +167,7 @@ func makeTitle(name, originName string) string {
 	return originName
 }
 
+// Tokens for
 type Tokens []Token
 
 func (ts Tokens) String(i int) string {
@@ -190,8 +177,6 @@ func (ts Tokens) String(i int) string {
 
 	return ts[i].OriginalName
 }
-
-func (ts Tokens) Len() int { return len(ts) }
 
 const cacheFileName = ".authycache.json"
 
