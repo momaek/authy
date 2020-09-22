e1be9efc44d625145937d73a12605fb357f53d6c
momaek <momaek17@gmail.com>
2020-09-22 21:09:06 +0800

    update release

---
+++ changelog.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+previous_tag=0
+for current_tag in $(git tag --sort=-creatordate)
+do
+
+if [ "$previous_tag" != 0 ];then
+    tag_date=$(git log -1 --pretty=format:'%ad' --date=short ${previous_tag})
+    printf "## ${previous_tag} (${tag_date})\n\n"
+    git log ${current_tag}...${previous_tag} --pretty=format:'*  %s [View](https://github.com/miclle/gsd/commit/%H)' --reverse | grep -v Merge
+    printf "\n\n"
+    break
+fi
+previous_tag=${current_tag}
+done
